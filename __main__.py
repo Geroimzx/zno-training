5e2c036f27e1ade6c9727a95403dee16a5fad1a0
Михайло Щербан <Mykhailo.Shcherban.KI.2020@edu.lpnu.ua>
2022-03-23 17:31:38 +0200

    bot v.0.0.1

--- __main__.py
+++ __main__.py
@@ -1,2 +1,24 @@
-class main:
-    print("Привіт Світ")
+import asyncio
+from aiogram import Bot, Dispatcher, types
+
+BOT_TOKEN = "5179947183:AAEomCwXUUSdkiaqBbu4Fbd_eCjN7ykknGU"
+
+
+async def start_handler(event: types.Message):
+    await event.answer(
+        f"Привіт, {event.from_user.get_mention(as_html=True)} 👋!",
+        parse_mode=types.ParseMode.HTML,
+    )
+
+
+async def main():
+    bot = Bot(token=BOT_TOKEN)
+    try:
+        disp = Dispatcher(bot=bot)
+        disp.register_message_handler(start_handler, commands={"start", "restart"})
+        await disp.start_polling()
+    finally:
+        await bot.close()
+
+
+asyncio.run(main())
