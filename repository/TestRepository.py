e912dfb826fa2aa4d69c0379fd2c64af0e800ad5
Михайло Щербан <Mykhailo.Shcherban.KI.2020@edu.lpnu.ua>
2022-04-08 22:31:56 +0300

    bot v.0.0.2

--- repository/TestRepository.py
+++ repository/TestRepository.py
@@ -4,6 +4,17 @@ class TestRepository:
 
     # Create query
 
+    # UserTest Create query
+    def createUserTest(self, user_test_id, user_id):
+        with self.conn.cursor() as cursor:
+            cursor.execute("INSERT INTO public.\"UserTest\"(\"UserTest_id\", \"User_id\") VALUES(%s, %s)",
+                           (user_test_id, user_id))
+
+    def createUserAnswer(self, user_answer_id, user_test_id, question_id, answer):
+        with self.conn.cursor() as cursor:
+            cursor.execute("INSERT INTO public.\"UserAnswer\"(\"UserAnswer_id\", \"Question_id\", \"UserTest_id\", \"Answer\") VALUES(%s, %s, %s, %s)",
+                           (user_answer_id, user_test_id, question_id, answer))
+
     # Read query
 
     # Subject Read query
@@ -30,7 +41,8 @@ class TestRepository:
 
     def existsYearSubjectId(self, subject_id):
         with self.conn.cursor() as cursor:
-            cursor.execute("SELECT EXISTS(SELECT \"Year\" FROM public.\"Year\" WHERE \"Subject_id\" = %s)", (subject_id,))
+            cursor.execute("SELECT EXISTS(SELECT \"Year\" FROM public.\"Year\" WHERE \"Subject_id\" = %s)",
+                           (subject_id,))
             return cursor.fetchone()[0]
 
     # Test Read query
@@ -42,12 +54,15 @@ class TestRepository:
     # Question Read query
     def findAllQuestionByTestId(self, test_id):
         with self.conn.cursor() as cursor:
-            cursor.execute("SELECT * FROM public.\"Question\" WHERE \"Test_id\" = %s \"ORDER BY QuestionNumber\"", (test_id,))
+            cursor.execute("SELECT * FROM public.\"Question\" WHERE \"Test_id\" = %s \"ORDER BY QuestionNumber\"",
+                           (test_id,))
             return cursor.fetchall()
 
     def findQuestionByTestIdAndQuestionNumber(self, test_id, question_number):
         with self.conn.cursor() as cursor:
-            cursor.execute("SELECT * FROM public.\"Question\" WHERE \"Test_id\" = %s AND \"Question_number\" = %s ORDER BY \"Question_number\"", (test_id, question_number,))
+            cursor.execute(
+                "SELECT * FROM public.\"Question\" WHERE \"Test_id\" = %s AND \"Question_number\" = %s ORDER BY \"Question_number\"",
+                (test_id, question_number,))
             return cursor.fetchall()
 
     # Answer Read query
@@ -56,14 +71,46 @@ class TestRepository:
             cursor.execute("SELECT * FROM public.\"Answer\" WHERE \"Question_number\" = %s", (question_number,))
             return cursor.fetchall()
 
-    # UserAnswer Read query TODO: test this
+    # UserAnswer Read query TODO: !!????????
     def findAllUserAnswerByUserTestId(self, user_test_id):
         with self.conn.cursor() as cursor:
-            cursor.execute("SELECT * FROM public.\"UserAnswer\" a WHERE \"UserTest_id\" = %s LEFT JOIN public.\"Question\" q ON a.\"Question_id\" = q.\"Question_id\"", (user_test_id,))
+            cursor.execute("SELECT * FROM public.\"UserAnswer\" WHERE \"UserTest_id\" = %s", (user_test_id,))
+            return cursor.fetchall()
+
+    def findAllUserAnswerByUserTestIdAndQuestionId(self, user_test_id, question_id):
+        with self.conn.cursor() as cursor:
+            cursor.execute("SELECT * FROM public.\"UserAnswer\" WHERE \"UserTest_id\" = %s AND \"Question_id\" = %s",
+                           (user_test_id, question_id,))
+            return cursor.fetchall()
+
+    # TODO: test this
+    def findAllUserAnswerByUserTestIdWithQuestion(self, user_test_id):
+        with self.conn.cursor() as cursor:
+            cursor.execute(
+                "SELECT * FROM public.\"UserAnswer\" a WHERE \"UserTest_id\" = %s LEFT JOIN public.\"Question\" q ON a.\"Question_id\" = q.\"Question_id\"",
+                (user_test_id,))
             return cursor.fetchall()
 
     # Update query
 
+    # UserTest Update query
+    def updateUserTestScore(self, user_test_id, score):
+        with self.conn.cursor() as cursor:
+            cursor.execute("UPDATE public.\"UserTest\" SET \"UserTest_id\" = %s, \"Score\" = %s",
+                           (user_test_id, score))
+
+    def updateUserTestStarted(self, user_test_id, date_started):
+        with self.conn.cursor() as cursor:
+            cursor.execute("UPDATE public.\"UserTest\" SET \"Test_started\" = %s WHERE \"UserTest_id\" = %s",
+                           (date_started, user_test_id))
+
+    def updateUserTestFinished(self, user_test_id, date_finished):
+        with self.conn.cursor() as cursor:
+            cursor.execute("UPDATE public.\"UserTest\" SET \"Test_finished\" = %s WHERE \"UserTest_id\" = %s",
+                           (date_finished, user_test_id))
+
+    def updateUserAnswer(self, user_answer_id, answer):
+        with self.conn.cursor() as cursor:
+            cursor.execute("UPDATE public.\"UserAnswer\" SET \"Answer\" = %s WHERE \"UserAnswer_id\" = %s",
+                           (answer, user_answer_id))
     # Delete query
-
-
