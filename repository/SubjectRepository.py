1b0cdd5547a88aecdb92de20eead8391d898cb8a
djighoul29 <51969410+djighoul29@users.noreply.github.com>
2022-04-10 17:19:36 +0300

    Update SubjectRepository.py

--- repository/SubjectRepository.py
+++ repository/SubjectRepository.py
@@ -5,3 +5,9 @@ class SubjectRepository:
     def addSubject(self, name):
         with self.conn.cusror() as cursor:
             cursor.execute(F"INSERT INTO public.\"Subject\"(\"Name\") VALUES ('{name}')")
+
+    def selectAllSubjects(self):
+        with self.conn.cusror() as cursor:
+            cursor.excute("SELECT * FROM public.\"Subject\"")
+            return cursor.fetchall()
+            
